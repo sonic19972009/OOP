8baf8c534ebea131ddc1fbb9fe01212dccb874d4
sonic19972009 <sonic19972009@yandex.ru>
2024-08-18 18:49:03 +0300

    3. Polymorphism and magic methods

--- main.py
+++ main.py
@@ -17,13 +17,26 @@ class Student:
         else:
             return 'Ошибка'
 
+    def average_grade(self):
+        result = 0
+        if self.grades:
+            for grade in self.grades.values():
+                result += sum(grade) / len(grade)
+            return result / len(self.grades)
+        else:
+            return 0
+
+    def __str__(self):
+        return (f"Имя:{self.name}\n Фамилия:{self.surname}\n Средняя оценка за домашние задания:"
+                f"{self.average_grade()}\n Курсы в процессе изучения:{self.courses_in_progress}\n Завершенные курсы:"
+                f"{self.finished_courses}")
+
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        self.grades = {}
 
 
 class Lecturer(Mentor):
@@ -32,12 +45,25 @@ class Lecturer(Mentor):
         self.courses_attached = []
         self.grades = {}
 
+    def average_grade(self):
+        total_grades = 0
+        for grade in self.grades:
+            total_grades += grade
+        result = total_grades / len(self.grades)
+        return result
+
+    def __gt__(self, other):
+        return self.average_grade() > other.average_grade()
+
+    def __str__(self):
+        return (f"Имя:{self.name}\n Фамилия:{self.surname}\n Средняя оценка за лекцию:"
+                f"{self.average_grade()}\n")
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.courses_attached = []
-        self.grades = {}
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -47,6 +73,9 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}\n"
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
